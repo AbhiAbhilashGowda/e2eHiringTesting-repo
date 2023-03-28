bd5641feaa571e0e925e807b5f635239e031a12b
AbhiAbhilashGowda <abhilashhs14@gmail.com>
2023-03-28 18:17:37 +0530

    adding automation files

--- src/test/java/com/automation/stepdef/CampusHiring.java
+++ src/test/java/com/automation/stepdef/CampusHiring.java
@@ -1,58 +1,57 @@
-//package com.automation.stepdef;
-//
-//import java.util.Iterator;
-//import java.util.Set;
-//
-//import org.openqa.selenium.JavascriptExecutor;
-//
-//import com.automation.base.Base;
-//import com.automation.framework.Elements;
-//import com.automation.pages.CampusHiringPage;
-//import com.automation.pages.MentorshipPage;
-//import com.automation.pages.UatloginPage;
-//
-//import cucumber.api.java.en.*;
-//
-//public class CampusHiring extends Base {
-//
-//	CampusHiringPage scenc = new CampusHiringPage();
-//	UatloginPage scenA = new UatloginPage();
-//
-//
-//@Then("^I click on more$")
-//public void i_click_on_more() throws Throwable {
-//    
-//}
-//
-//@Then("^I click on campus$")
-//public void i_click_on_campus() throws Throwable {
-//
-//}
-//
-//@Then("^I click on register$")
-//public void i_click_on_register() throws Throwable {
-//    
-//}
-//
-//@Then("^I enter last name \"([^\"]*)\"$")
-//public void i_enter_last_name(String arg1) throws Throwable {
-//
-//}
-//
-//@Then("^I enter mailid in register form \"([^\"]*)\"$")
-//public void i_enter_mailid_in_register_form(String arg1) throws Throwable {
-//
-//}
-//
-//@Then("^I enter password in register form \"([^\"]*)\"$")
-//public void i_enter_password_in_register_form(String arg1) throws Throwable {
-//    
-//}
-//
-//@Then("^I click on register button$")
-//public void i_click_on_register_button() throws Throwable {
-//
-//}
-//
-//
-//}
+package com.automation.stepdef;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+import com.automation.base.Base;
+import com.automation.framework.Elements;
+import com.automation.pages.CampusHiringPage;
+import com.automation.pages.UatloginPage;
+
+import cucumber.api.java.en.*;
+
+public class CampusHiring extends Base {
+
+	CampusHiringPage scenc = new CampusHiringPage();
+	UatloginPage scenA = new UatloginPage();
+
+
+@Then("^I click on more$")
+public void i_click_on_more() throws Throwable {
+    
+}
+
+@Then("^I click on campus$")
+public void i_click_on_campus() throws Throwable {
+
+}
+
+@Then("^I click on register$")
+public void i_click_on_register() throws Throwable {
+    
+}
+
+@Then("^I enter last name \"([^\"]*)\"$")
+public void i_enter_last_name(String arg1) throws Throwable {
+
+}
+
+@Then("^I enter mailid in register form \"([^\"]*)\"$")
+public void i_enter_mailid_in_register_form(String arg1) throws Throwable {
+
+}
+
+@Then("^I enter password in register form \"([^\"]*)\"$")
+public void i_enter_password_in_register_form(String arg1) throws Throwable {
+    
+}
+
+@Then("^I click on register button$")
+public void i_click_on_register_button() throws Throwable {
+
+}
+
+
+}
