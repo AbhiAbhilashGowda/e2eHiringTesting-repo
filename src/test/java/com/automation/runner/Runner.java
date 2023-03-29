2aeb97055be9ed00c3859fc4fc1d209b629b60a8
AbhiAbhilashGowda <abhilashhs14@gmail.com>
2023-03-29 17:27:25 +0530

    Raghu jain

--- src/test/java/com/automation/runner/Runner.java
+++ src/test/java/com/automation/runner/Runner.java
@@ -7,21 +7,13 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"classpath:FeatureFiles/Uatlogin.feature",
-<<<<<<< HEAD
-							"classpath:FeatureFiles/Jobs.feature"
+		                   "classpath:FeatureFiles/CampusHiring.feature",
+		                     "classpath:FeatureFiles/Mentorship.feature"
 						  },
 glue={"classpath:com.automation.stepdef"},
 plugin={"html:target/cucumber_html_report"})
 //tags= {"@Jobss","@one"})
-=======
-		                   //"classpath:FeatureFiles/Mentorship.feature",
-	                     	//"classpath:FeatureFiles/CampusHiring.feature",
-		                    "classpath:FeatureFiles/Jobs.feature"
-						  },
-glue={"classpath:com.automation.stepdef"},
-plugin={"html:target/cucumber_html_report"},
-tags= {"@Jobss","@one"})
->>>>>>> 68fb9eb6dfe0e53016909ab4e54b6ccebf4a57e6
+
 
 public class Runner {
 	
